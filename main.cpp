0f79b622692460ccc6514c13bba930a5822ea8b4
Wendig0x <wendig0x@protonmail.com>
2022-02-12 16:44:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,56 @@
-﻿#include <Windows.h>
+﻿
+#include <Windows.h>
+#include <iostream>
+#include "winternl.h"
 #include "advanced_caller.h"
+
+using namespace std;
+
+typedef void (WINAPI* _RtlInitUnicodeString)(
+	PUNICODE_STRING DestinationString,
+	PCWSTR SourceString
+	);
+
 
 int main() {
 
-	
-	NTSTATUS status;
+
+	WCHAR chDmpFile[MAX_PATH] = L"\\??\\";
+	WCHAR chWinPath[MAX_PATH];
+
+	GetCurrentDirectory(MAX_PATH, chWinPath);
+
+	wcscat_s(chDmpFile, sizeof(chDmpFile) / sizeof(wchar_t), chWinPath);
+	wcscat_s(chDmpFile, sizeof(chDmpFile) / sizeof(wchar_t), L"\\test_file777.txt");
+
+	_RtlInitUnicodeString RtlInitUnicodeString = (_RtlInitUnicodeString)
+		GetProcAddress(GetModuleHandle(L"ntdll.dll"), "RtlInitUnicodeString");
+
+	if (RtlInitUnicodeString == nullptr) {
+		return 0;
+	}
+
+	UNICODE_STRING uFileName;
+	RtlInitUnicodeString(&uFileName, chDmpFile);
+
+	HANDLE hTestFile = NULL;
+	IO_STATUS_BLOCK IoStatusBlock;
+
+	ZeroMemory(&IoStatusBlock, sizeof(IoStatusBlock));
+	OBJECT_ATTRIBUTES FileObjectAttributes;
+	InitializeObjectAttributes(&FileObjectAttributes, &uFileName, OBJ_CASE_INSENSITIVE, NULL, NULL);
+
 	SysCall directcall;
-	
 
-//	if(NT_SUCCESS(status)) ...
-
-    return 0;
-}
+	NTSTATUS status = directcall.call("ZwCreateFile",
+		&hTestFile,
+		FILE_GENERIC_WRITE,
+		&FileObjectAttributes,
+		&IoStatusBlock,
+		0,
+		FILE_ATTRIBUTE_NORMAL,
+		FILE_SHARE_READ | FILE_SHARE_WRITE,
+		FILE_OVERWRITE_IF,
+		FILE_SYNCHRONOUS_IO_NONALERT,
+		NULL,
+		0);
